932fbd2a9bfb94399f166ab87e874e14d1e34cef
RafaCypherpunk <rafafuentesrangel@outlook.com>
2022-01-19 22:13:22 -0600

    Adding more event.params for transferedOwnership

--- src/mapping.ts
+++ src/mapping.ts
@@ -8,5 +8,10 @@ import {
 } from "../generated/schema"
 
 export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void {
-  let token = Token.load(event.params.tokenId.toString());
+  let token = Token.load(event.params.previousOwner.toString());
+  if(!token) {
+    token = new Token(event.params.previousOwner.toHexString());
+    token.creator = event.params.newOwner.toHexString();
+    token.name = event.params.name.toString();
+  }
 }
